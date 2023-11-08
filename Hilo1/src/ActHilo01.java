831637c746583903a1556d22722b0a014f4823b7
byemidev <emilio.arevalozuniga@gmail.com>
2023-11-08 23:28:40 +0100

    ActHilo01 terminada! con comentarios incluido

--- Hilo1/src/ActHilo01.java
+++ Hilo1/src/ActHilo01.java
@@ -7,10 +7,6 @@ class ActHilo01 extends Thread{
     public ActHilo01(String nombre) {
         this.nombre = nombre;
     }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
     public String getNombre() {
         return nombre;
     }
@@ -23,28 +19,32 @@ class ActHilo01 extends Thread{
     @Override
     public void run() {
         int numRand;
-        numRand = (int) (Math.random() * 8) + 3; //genero un numero aleatorio entre 3 y 8
+        numRand = (int) (Math.random() * 8) + 3; //genero un numero aleatorio entre 3 y 8 como longitud del bucle
         for(int i = 0; i < numRand; i++){
-            try {
+            try { //genero un numero aleatorio entre 1 y 7, estara dentro del for por que me interesa que duerma cada proceso de forma aleatoria por cada iteracion en el bucle
                 int tiempo = (int)((Math.random() * 7) +1) * 1000;
                 sleep(tiempo);
                 System.out.println("Mi nombre es " + getNombre() + "he domido ZzzZ --> " + tiempo +
                         " segundos");
 
-            } catch (InterruptedException e) {
+            } catch (InterruptedException e) { //sleep arroja una exception, puede que un hilo no pueda dormirse si estan usando algo que necesita o que lo haya hecho antes de tiempo.
                 System.err.println("error de interrupcion...");
             }
         }
     }
 
     public static void main(String[] args) {
+        //Creo una lista de hilos
         List<ActHilo01> lsHilos = new ArrayList<>();
+        //Creo una lista de nombres para cada hilo
         String [] lsNombres = {"BlackMelon", "YellowChocolate", "WitheOrange", "BlueBubble", "GreenPeace"};
-        for (String n:
-             lsNombres) {
+
+        //por cada  nombre creo una instancia de mi clase ActHilo01 == Thread
+        for (String n : lsNombres) {
             lsHilos.add(new ActHilo01(n));
         }
 
+        //usamos el metodo start() por cada hilo de mi lista de hilos.
         for(ActHilo01 hilo : lsHilos ){
             hilo.start();
         }
