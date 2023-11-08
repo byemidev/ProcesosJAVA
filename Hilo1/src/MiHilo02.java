43d280f8c1d8f0269bcd229f2172944b298605c6
byemidev <alumnotv@max11>
2023-11-08 21:04:48 +0100

    commit desde clase

---
+++ Hilo1/src/MiHilo02.java
@@ -0,0 +1,22 @@
+class MiHilo02 extends Thread{
+    private String nombre;
+    MiHilo02(String nombre){
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void run(){
+        System.out.println(this.nombre);
+    }
+
+    public static void main(String[] args) {
+
+        MiHilo02 hilo1 = new MiHilo02("hilo1");
+        MiHilo02 hilo2 = new MiHilo02("hilo2");
+        MiHilo02 hilo3 = new MiHilo02("hilo3");
+
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+    }
+}
---
+++ Hilos01/src/Hilos1.java
@@ -0,0 +1,19 @@
+//ejemplo clase Hilos1
+public class MiHilo() extends Thread{
+    //este metodo
+    public void run(){//run
+        try{
+            for (int i = 0; i <=5; i++){
+                System.out.println(i);
+                Thread.sleep(1000); //dormmir por un segundo
+            }
+        }catch (InterruptedException ie){
+            System.err.println("error >> el hijo fue interrumpido " + ie.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {//main
+        MiHilo hilo1 = new MiHilo();
+
+    }
+}
