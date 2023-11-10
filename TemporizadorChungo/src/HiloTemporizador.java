432dc72c97307567a117f29b51febb104bc66d5e
byemidev <97583628+byemidev@users.noreply.github.com>
2023-11-10 23:39:48 +0100

    Update HiloTemporizador.java
    HiloTeMporizador basic

--- TemporizadorChungo/src/HiloTemporizador.java
+++ TemporizadorChungo/src/HiloTemporizador.java
@@ -1,2 +1,24 @@
-public class HiloTemporizador {
+public class HiloTemporizador extends Thread {
+    private String nombre; 
+    private Dato dato; 
+
+    public HiloTemporizador(){ //constructor de hilo 
+        
+    }
+    public static run(){
+        //Necesito que cada 30 segundos 
+        int treinta = 30; 
+        long timeMillis = System.currentTimeMillis();
+        long timeSeconds = TimeUnit.MILLISECONDS.toSeconds(timeMillis);
+        //if timeSeconds%30 == 0 han pasado 30. segundos ? 
+        while(timeSeconds%30 != 0){
+            if(acabado){
+                break;
+            }
+            //estado = finalizado; 
+            Thread.sleep(1000);
+        }
+    }
+    
+    
 }
