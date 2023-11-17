6ae03eb270cd62cdf5e97b199458eeca08c0b3cd
byemidev <emilio.arevalozuniga@gmial.com>
2023-11-17 20:07:52 +0100

    Adding comprimido ejemplo. Init ejemplo cocina , mejorando programa cocina visto en clase.

---
+++ cocina/src/Comanda.java
@@ -0,0 +1,31 @@
+package cocina;
+
+
+public class Comanda extends Thread{
+    
+    public void run()
+    {
+        Menu menu;
+        Cocina cocina;
+
+        while(!Thread.interrupted())
+        {
+           menu=new Menu();
+           cocina=new Cocina(menu.platosAleatorios());
+            cocina.start();
+            //esperamos a que termine de prepara los platos
+            try {
+                cocina.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                System.out.println("Cerramos comandas");
+                cocina.interrupt();
+                break;
+            }
+    
+        }
+        System.out.println("Cerramos comandas");
+        
+    }
+}
+
